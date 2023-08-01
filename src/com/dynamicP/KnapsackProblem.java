8a025eb74c91e0a8377ea8bf5f79c30a9c5cb0ab
Riyaz H A <rafthab@salesforce.com>
2023-08-01 05:28:58 -0700

    july 31

--- src/com/dynamicP/KnapsackProblem.java
+++ src/com/dynamicP/KnapsackProblem.java
@@ -2,33 +2,26 @@ package com.dynamicP;
 
 class KnapsackProblem {
     public static int knapsackRecursive(int[][] lookupTable, int profits[], int profitsLength, int weights[], int weightsLength, int capacity, int currentIndex) {
-
-        // base checks
-        if (capacity <= 0 || currentIndex >= profitsLength || currentIndex < 0 || weightsLength != profitsLength)
+        if(capacity<=0 || currentIndex>= profitsLength || currentIndex <0 || weightsLength != profitsLength)
             return 0;
+        // if we already solved the problem then return the result from the table
+        if (lookupTable[currentIndex][capacity] !=0)
+            return lookupTable[currentIndex][capacity];
 
-        // if we have already solved the problem, return the result from the table
-        if (lookupTable[currentIndex][capacity] != 0) return lookupTable[currentIndex][capacity];
-
-        // recursive call after choosing the element at the currentIndex
-        // if the weight of the element at currentIndex exceeds the capacity, we shouldn't process this
+        // recursive call after choosing the element at the current index
+        // if the weight of the element at the currentindex exceeds the capacity , we shouldnt process this
         int profit1 = 0;
-        if (weights[currentIndex] <= capacity)
-            profit1 = profits[currentIndex] + knapsackRecursive(lookupTable, profits, profitsLength, weights, weightsLength, capacity - weights[currentIndex], currentIndex + 1);
-
-        // recursive call after excluding the element at the currentIndex
-        int profit2 = knapsackRecursive(lookupTable, profits, profitsLength, weights, weightsLength, capacity, currentIndex + 1);
-
-        lookupTable[currentIndex][capacity] = Math.max(profit1, profit2);
-        return lookupTable[currentIndex][capacity];
+        if(weights[currentIndex] <= capacity)
+            profit1 = profits[currentIndex] + knapsackRecursive(lookupTable, profits, profitsLength, weights, weightsLength, capacity - weights[currentIndex], currentIndex+1);
+        int profit2 = knapsackRecursive(lookupTable, profits, profitsLength, weights, weightsLength, capacity, currentIndex+1);
+        return lookupTable[currentIndex][capacity] = Math.max(profit1, profit2);
     }
 
     public static int knapSack(int profits[], int profitsLength, int weights[], int weightsLength, int capacity) {
         int lookupTable[][] = new int[profitsLength][];
-
-        for (int i = 0; i < profitsLength; i++) {
-            lookupTable[i] = new int[capacity + 1];
-            for (int j = 0; j < capacity + 1; j++)
+        for(int i=0; i<profitsLength; i++) {
+            lookupTable[i] = new int[capacity +1];
+            for(int j=0; j<capacity +1; j++)
                 lookupTable[i][j] = 0;
         }
         return knapsackRecursive(lookupTable, profits, profitsLength, weights, weightsLength, capacity, 0);
