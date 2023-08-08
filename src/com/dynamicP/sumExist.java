39b09e6030ddf2676efdfaf900a8abdd790311a9
Riyaz H A <rafthab@salesforce.com>
2023-08-08 07:26:26 -0700

    changes july end

--- src/com/dynamicP/sumExist.java
+++ src/com/dynamicP/sumExist.java
@@ -2,30 +2,14 @@ package com.dynamicP;
 
 import java.util.HashMap;
 
-/**
- * Problem statement
- * Given an array of integers, write a function to find if any two subsets of the input array exist such that the sum of both subsets is equal. You can assume that the array will only consist of positive integers.
- *
- * Input
- * An array of positive integers.
- *
- * Output
- * The output is a boolean. It will be True if such subsets exist and False if they do not.
- *
- * Sample input
- * int set[] = {1, 2, 3, 4};
- * Sample output
- * true          // (The 2 subsets will be 1,4 & 2,3)
- */
-public class PartitionProblem {
+
+public class sumExist {
 
     // Solution 1: Brute force
     // Time Complexity: O(n^2)
     // Space Complexity: O(n)
     // n = number of elements in the array
-    public static boolean partitionProblem(int[] set) {
-        return false;
-    }
+
 
     public static boolean canPartition(int[]num, HashMap<Integer, Integer> map)
     {
