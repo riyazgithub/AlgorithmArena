55999f28d7b3580df0e03983fa49d7a5ae1aeb6c
Riyaz H A <rafthab@salesforce.com>
2023-09-18 10:36:57 -0700

    Refactoring

--- src/main/java/com/ds/array/SnakeGame.java
+++ src/main/java/com/ds/array/SnakeGame.java
@@ -2,18 +2,25 @@ package com.ds.array;
 
 import java.util.Deque;
 import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
+
 
 /**
- * Design a logger system that receives a stream of messages along with their timestamps. Each unique message should only be printed at most every 10 seconds (i.e. a message printed at timestamp t will prevent other identical messages from being printed until timestamp t + 10).
- *
- * All messages will come in chronological order. Several messages may arrive at the same timestamp.
- *
- * Implement the Logger class:
- *
- * Logger() Initializes the logger object.
- * bool shouldPrintMessage(int timestamp, string message) Returns true if the message should be printed in the given timestamp, otherwise returns false.
+ Design a Snake game that is played on a device with screen size height x width. Play the game online if you are not familiar with the game.
+
+ The snake is initially positioned at the top left corner (0, 0) with a length of 1 unit.
+
+ You are given an array food where food[i] = (ri, ci) is the row and column position of a piece of food that the snake can eat. When a snake eats a piece of food, its length and the game's score both increase by 1.
+
+ Each piece of food appears one by one on the screen, meaning the second piece of food will not appear until the snake eats the first piece of food.
+
+ When a piece of food appears on the screen, it is guaranteed that it will not appear on a block occupied by the snake.
+
+ The game is over if the snake goes out of bounds (hits a wall) or if its head occupies a space that its body occupies after moving (i.e. a snake of length 4 cannot run into itself).
+
+ Implement the SnakeGame class:
+
+ SnakeGame(int width, int height, int[][] food) Initializes the object with a screen of size height x width and the positions of the food.
+ int move(String direction) Returns the score of the game after applying one direction move by the snake. If the game is over, return -1.
  */
 class SnakeGame {
     class point {
---
+++ src/test/java/com/ds/array/RankTEamsByVotesTest.java
@@ -0,0 +1,36 @@
+package com.ds.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RankTEamsByVotesTest {
+    @Test
+    public void testRankTeams() {
+        RankTEamsByVotes solution = new RankTEamsByVotes();
+
+        // Test case 1: Single vote, single team
+//        String[] votes1 = { "A" };
+//        assertEquals("A", solution.rankTeams(votes1));
+//
+//        // Test case 2: Single vote, multiple teams
+//        String[] votes2 = { "ABCD" };
+//        assertEquals("ABCD", solution.rankTeams(votes2));
+//
+//        // Test case 3: Multiple votes with the same order
+//        String[] votes3 = { "ABCD", "ABCD", "ABCD" };
+//        assertEquals("ABCD", solution.rankTeams(votes3));
+
+        // Test case 4: Multiple votes with different orders
+        String[] votes4 = { "ABC","ACB","ABC","ACB","ACB" };
+        assertEquals("ACB", solution.rankTeams(votes4));
+//
+//        // Test case 5: Teams with the same score
+//        String[] votes5 = { "ABCB", "CBAB", "BACA" };
+//        assertEquals("ABCB", solution.rankTeams(votes5));
+//
+//        // Test case 6: Teams with different scores
+//        String[] votes6 = { "ABCC", "CBAA", "BACA" };
+//        assertEquals("ACB", solution.rankTeams(votes6));
+    }
+}
