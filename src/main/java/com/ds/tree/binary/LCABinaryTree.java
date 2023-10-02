74006db0c81f5d13f68b27a79293362199d90e16
Riyaz H A <rafthab@salesforce.com>
2023-10-02 06:08:35 -0700

    Create LCABinaryTree.java

---
+++ src/main/java/com/ds/tree/binary/LCABinaryTree.java
@@ -0,0 +1,13 @@
+package com.ds.tree.binary;
+
+import javax.swing.tree.TreeCellRenderer;
+
+public class LCABinaryTree {
+    TreeNode lca;
+    public static void main(String[] args) {
+        Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        TreeNode root = TreeUtils.arrayToTree(arr);
+        TreeUtils.printTreeGraphics(root);
+        LCABinaryTree
+    }
+}
