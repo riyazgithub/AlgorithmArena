2708cf1461c83d049b9f144f5c976038e2a5f2a1
Riyaz H A <rafthab@salesforce.com>
2023-10-02 15:03:30 -0700

    Update LCABinaryTree.java

--- src/main/java/com/ds/tree/binary/LCABinaryTree.java
+++ src/main/java/com/ds/tree/binary/LCABinaryTree.java
@@ -4,10 +4,32 @@ import javax.swing.tree.TreeCellRenderer;
 
 public class LCABinaryTree {
     TreeNode lca;
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) {
+            return root;
+        }
+        lowestCommonAncestor_help(root, p, q);
+        return this.lca;
+    }
+
+    public boolean lowestCommonAncestor_help(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) {
+            return false;
+        }
+        int l = lowestCommonAncestor_help(root.left, p, q) ? 1 : 0;
+        int r = lowestCommonAncestor_help(root.right, p, q) ? 1 : 0;
+        int m = (root.val == p.val || root.val == q.val) ? 1 : 0;
+        if(l+r+m >= 2) {
+            this.lca = root;
+        }
+        return (l+r+m > 0);
+    }
+
     public static void main(String[] args) {
         Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
         TreeNode root = TreeUtils.arrayToTree(arr);
         TreeUtils.printTreeGraphics(root);
-        LCABinaryTree
+        LCA
     }
 }
